1a247507860d57fd08d6eba994be52f2cda1a287
Bash_Grey <yesufubashir@gmail.com>
2022-04-05 14:52:16 +0100

    use date time to get day of the week

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import smtplib
+# import smtplib
+#
+# my_email = "your email"
+# password = "your password"
+#
+# with smtplib.SMTP('smtp.gmail.com') as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password,)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs='receiver email',
+#         msg='Subject:Hello\n\nThis is the body of my mail. New email'
+#     )
 
-my_email = "your email"
-password = "your password"
+import datetime as dt
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password,)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs='receiver email',
-        msg='Subject:Hello\n\nThis is the body of my mail. New email'
-    )
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
 
+date_of_birth = dt.datetime(year=1995, month=12, day=15)
